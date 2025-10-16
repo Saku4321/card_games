bf0f8cef8e0ce38d1c89922e3e2c967d79bac2bd
Jakub <01197854@pw.edu.pl>
2025-10-16 13:36:53 +0200

    Wprowadzono mozliwosc splitu dłoni

--- Black_jack.py
+++ Black_jack.py
@@ -21,12 +21,12 @@ class Deck:
     def deal(self):
         return self.cards.pop()
 
-class Hand():
+class Hand:
     def __init__(self):
         self.cards = []
         self.value = 0
         self.aces = 0
-    def addCard(self, card):
+    def addcard(self, card):
         self.cards.append(card)
         self.value += card.value
         if card.rank == 'Ace':
@@ -36,7 +36,7 @@ class Hand():
         while self.value > 21 and self.aces:
             self.value -= 10
             self.aces -= 1
-class Chips():
+class Chips:
     def __init__(self):
         self.total = 0
         self.bet = 0
@@ -63,6 +63,7 @@ class Player(Hand,Chips):
         self.isblackjack = False
     def play_hand(self,deck):
         self.isblackjack = False
+        second_hand = None
         while True:
             bet_to_take = input("How many chips would you like to bet (min 25 and must be n*25)?")
             if bet_to_take.isdigit() and int(bet_to_take) >= 25 and int(bet_to_take)%25 ==0 and int(bet_to_take) <= self.total:
@@ -71,19 +72,54 @@ class Player(Hand,Chips):
                 print("Invalid input, try again.")
                 continue
         self.take_bet(int(bet_to_take))
-        self.addCard(card_(deck))
+        self.addcard(card_(deck))
         print(self.cards[-1])
-        self.addCard(card_(deck))
+        self.addcard(card_(deck))
         print(self.cards[-1])
         print(self.value)
-        if(self.value == 21):
+        if self.can_split():
+            option = input("Would you like to split the hand? (y/n)")
+            if option == 'y':
+                second_hand = self.split(deck)
+                print("You choose to split the hand.")
+                self.total-=self.bet
+            else:
+                print("You choose not to split.")
+
+        print(f"Playing first hand ({[str(c) for c in self.cards]}):")
+        self.play_individual_split_hand(deck)
+        if second_hand:
+            print("Now playing second hand")
+            self.play_individual_split_hand(deck.second_hand)
+
+        if self.value == 21:
             print(f'{self.name} BlackJack!')
             self.isblackjack = True
-        while self.value < 21:
-            print(f"{self.name} value is {self.value}")
-            option = input("Would you like to take card y/n?")
+        return second_hand
+
+    def can_split(self):
+        return len(self.cards)==2 and self.cards[0].rank == self.cards[1].rank and self.total >= self.bet
+
+    def split(self,deck):
+        second_hand = Hand()
+        second_hand.addcard(self.cards.pop())
+        self.value = self.cards[0].value
+
+        self.addcard(card_(deck))
+        second_hand.addcard(card_(deck))
+        print(f"First hand: {[str(c) for c in self.cards]} (value={self.value})")
+        print(f"Second hand: {[str(c) for c in second_hand.cards]} (value={second_hand.value})")
+
+        return second_hand
+
+    def play_individual_split_hand(self,deck,hand=None):
+        if hand is None:
+            hand = self
+        while hand.value < 21:
+            print(f"Current value {hand.value}")
+            option = input("Would you like to take the card? (y/n)")
             if option == 'y':
-                self.addCard(card_(deck))
+                self.addcard(card_(deck))
                 print(self.cards[-1])
                 print(self.value)
             elif option == 'n':
@@ -91,27 +127,29 @@ class Player(Hand,Chips):
             else:
                 print("Invalid input, try again.")
 
+
+
 class Dealer(Player):
     def __init__(self):
         Player.__init__(self, 'Dealer')
 
     def play_deal(self,deck, playervalue):
         self.isblackjack = False
-        self.addCard(card_(deck))
+        self.addcard(card_(deck))
         print(self.cards[-1])
-        self.addCard(card_(deck))
+        self.addcard(card_(deck))
         print(self.cards[-1])
         print(self.value)
-        if(self.value == 21):
+        if self.value == 21:
             print('Dealer have BlackJack!')
             self.isblackjack = True
         while self.value < 17:
-            self.addCard(card_(deck))
+            self.addcard(card_(deck))
             print(self.cards[-1])
             print(self.value)
 
-        while self.value < 21 and playervalue > self.value and playervalue <21:
-            self.addCard(card_(deck))
+        while self.value < 21 and self.value < playervalue < 21:
+            self.addcard(card_(deck))
             print(self.cards[-1])
             print(self.value)
 
@@ -145,7 +183,7 @@ while True:
     dealer1.is_blackjack = False
     deck = Deck()
     deck.shuffle()
-    player1.play_hand(deck)
+    second_hand = player1.play_hand(deck)
     # 1️⃣ Sprawdź, czy gracz przekroczył 21
     if player1.value > 21:
         print(f'{player1.name} busts (over 21)! Dealer wins.')
@@ -178,6 +216,21 @@ while True:
         else:
             print("DRAW (same value).")
             player1.draw_bet()
+
+        if second_hand:
+            print("\n--- Checking second hand ---")
+            if second_hand.value > 21:
+                print(f"{player1.name} busts on second hand!")
+            else:
+                dealer1.play_deal(deck, second_hand.value)
+                if dealer1.value > 21 or second_hand.value > dealer1.value:
+                    print(f"{player1.name} wins on second hand!")
+                    player1.win_bet()
+                elif second_hand.value < dealer1.value:
+                    print("Dealer wins on second hand.")
+                else:
+                    print("Second hand draw.")
+                    player1.draw_bet()
     # 🔁 zapytanie o kontynuację
     x = input("Do you want to continue? (y/n): ").lower()
     if x == 'y':
